e5b91d5008ddd824b863ad9bd35ecca110e6a7e2
Vldr22 <vldr2212l@gmail.com>
2025-11-11 21:59:24 +0300

    Реализация серверного слоя CRUD управления файлами

---
+++ src/main/java/org/resume/s3filemanager/repository/FileMetadataRepository.java
@@ -0,0 +1,22 @@
+package org.resume.s3filemanager.repository;
+
+import jakarta.transaction.Transactional;
+import org.resume.s3filemanager.entity.FileMetadata;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface FileMetadataRepository extends JpaRepository<FileMetadata, Long> {
+
+    boolean existsByFileHash(String fileHash);
+
+    Optional<FileMetadata> findByOriginalName(String originalName);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM FileMetadata f WHERE f.uniqueName = :uniqueName")
+    int deleteByUniqueName(@Param("uniqueName") String uniqueName);
+}
