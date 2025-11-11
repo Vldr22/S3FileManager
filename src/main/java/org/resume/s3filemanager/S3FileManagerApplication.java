0c724d8420d9bb2c0eebd2262cd4017675790838
Vldr22 <vldr2212l@gmail.com>
2025-11-11 16:22:46 +0300

    Initial commit: Сборка Spring boot проекта

---
+++ src/main/java/org/resume/s3filemanager/S3FileManagerApplication.java
@@ -0,0 +1,13 @@
+package org.resume.s3filemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3FileManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3FileManagerApplication.class, args);
+    }
+
+}
