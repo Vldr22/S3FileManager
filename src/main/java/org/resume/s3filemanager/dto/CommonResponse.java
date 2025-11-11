6b5a76feeecbe33a49eb8e893f7b91099c5bde67
Vldr22 <vldr2212l@gmail.com>
2025-11-11 21:58:51 +0300

    Созданий общего response класса

---
+++ src/main/java/org/resume/s3filemanager/dto/CommonResponse.java
@@ -0,0 +1,39 @@
+package org.resume.s3filemanager.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.ProblemDetail;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommonResponse<T> {
+
+    private T data;
+    private ResponseStatus status;
+    private ProblemDetail problemDetail;
+    private LocalDateTime timestamp;
+
+    public static <T> CommonResponse<T> success(T data) {
+         return new CommonResponse<>(
+                 data,
+                 ResponseStatus.SUCCESS,
+                 null,
+                 LocalDateTime.now());
+    }
+
+    public static <T> CommonResponse<T> error(ProblemDetail problemDetail) {
+        return new CommonResponse<>(
+                null,
+                ResponseStatus.SUCCESS,
+                problemDetail,
+                LocalDateTime.now());
+    }
+
+}
