2158ec6e56bc52318b438ebe774272f6d6bc63be
Vldr22 <vldr2212l@gmail.com>
2025-12-08 17:44:05 +0300

    refactor: оптимизация logging и исключений в сервисах
    - Убрано дублирование логов между слоями сервисов и exceptionHandler
    - Добавлена @ValidFile на параметр file
    - Замена EntityNotFoundException на кастомные exceptions

--- src/main/java/org/resume/s3filemanager/service/auth/UserService.java
+++ src/main/java/org/resume/s3filemanager/service/auth/UserService.java
@@ -1,13 +1,12 @@
 package org.resume.s3filemanager.service.auth;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.resume.s3filemanager.constant.ErrorMessages;
 import org.resume.s3filemanager.entity.User;
 import org.resume.s3filemanager.enums.FileUploadStatus;
 import org.resume.s3filemanager.enums.UserRole;
 import org.resume.s3filemanager.exception.UserAlreadyExistsException;
+import org.resume.s3filemanager.exception.UserNotFoundException;
 import org.resume.s3filemanager.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,7 +21,6 @@ public class UserService {
 
     public void createUser(String username, String password) {
         createUserWithRole(username, password, UserRole.USER, FileUploadStatus.NOT_UPLOADED);
-        log.info("User created: {}", username);
     }
 
     public void createAdmin(String username, String password) {
@@ -33,7 +31,7 @@ public class UserService {
     private void createUserWithRole(String username, String password,
                                     UserRole role, FileUploadStatus uploadStatus) {
         if (existsByUsername(username)) {
-            throw new UserAlreadyExistsException(ErrorMessages.USER_ALREADY_EXISTS);
+            throw new UserAlreadyExistsException();
         }
 
         User user = new User();
@@ -47,8 +45,10 @@ public class UserService {
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username)
-                .orElseThrow(() -> new EntityNotFoundException(
-                        String.format(ErrorMessages.USER_NOT_FOUND, username)));
+                .orElseThrow(() -> {
+                    log.warn("User not found: {}", username);
+                    return new UserNotFoundException(username);
+                });
     }
 
     public boolean existsByUsername(String username) {
